c6afa814c8347c5f64578073574d7e1f61e1e80f
Bart <barttokarz@gmail.com>
2019-02-26 18:57:57 -0500

    config file update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '100 Days of Code Blog', // Required
   author: 'Bart Tokarz', // Required
   description: 'Web Developer',
-  primaryColor: '#3498db', // Required
+  primaryColor: '#1669a0', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
