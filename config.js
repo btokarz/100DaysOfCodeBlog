fa6a91a493b0e2ec009d291b33f619d364499d00
Bart <barttokarz@gmail.com>
2019-02-26 18:48:19 -0500

    config file changes

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Bart Tokarz', // Required
+  description: 'Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://btokarz.com',
+    github: 'https://github.com/btokarz',
+    linkedin: 'https://www.linkedin.com/in/btokarz',
   },
   pathPrefix: '/devblog',
   siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
