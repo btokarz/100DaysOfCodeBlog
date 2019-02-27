233e2b07c6634588b7146e55c1bda90fc5227e2e
Bart <barttokarz@gmail.com>
2019-02-27 18:17:05 -0500

    update config with new domain

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '100 Days of Code Blog', // Required
   author: 'Bart Tokarz', // Required
   description: 'Web Developer',
-  primaryColor: '#1669a0', // Required
+  primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
@@ -11,6 +11,6 @@ module.exports = {
     github: 'https://github.com/btokarz',
     linkedin: 'https://www.linkedin.com/in/btokarz',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://upbeat-brown-f11072.netlify.com/',
+  pathPrefix: '/100DaysOfCodeBlog',
+  siteUrl: 'https://btokarz-blog.netlify.com/',
 };
